95d3c2e251351ff66619bb037c80b95180fd497a
Darshil-Papalkar <papalkardarshil13@gmail.com>
2022-01-16 18:25:42 +0530

    backup v1.1

--- src/components/MusicPlayer/index.js
+++ src/components/MusicPlayer/index.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Nav, Container } from "reactstrap";
+
+import PauseIcon from '@mui/icons-material/Pause';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 
 import "./index.css";
 
 const MusicPlayer = (props) => {
 
+    const [playing, setPlaying] = useState(false);
+
     return(
         <>
 
-            <div className="mb-5 navigation-container">
-                <Navbar 
-                    color="dark"
-                    dark
-                    expand
-                    fixed="bottom"
-                    full
-                    light
-                    className="top-one-row"
+            <div className="mt-2 bottom-navigation-container">
+                <div 
+                    // color="dark"
+                    // dark
+                    // expand
+                    // fixed="bottom"
+                    // full
+                    // light
+                    className="top-one-row custom-bottom-navigation"
                 >
                     <div className="image-box">
                         <div className="navbarHead-img-container">
@@ -32,13 +39,22 @@ const MusicPlayer = (props) => {
                             Some, More New artist which you can imagine whatever you want just imagine
                         </h6>
                     </div>
-                    
-                        <Nav className="ms-auto" navbar>
-                            
-                        </Nav>
-                </Navbar>
+
+
+                    <div className="custom-button-group-container">
+                        <div className="custom-button-group">
+                            <SkipPreviousIcon fontSize="large" />
+                            {
+                                playing ?
+                                <PauseIcon fontSize="large" /> : 
+                                <PlayArrowIcon fontSize="large" />
+                            }
+                            <SkipNextIcon fontSize="large" />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <Container className="pt-5 pb-5"/>
+            {/* <Container className="pt-5 pb-5"/> */}
         </>
     );
 };
